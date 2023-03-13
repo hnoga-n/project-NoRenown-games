8d4f53457bdc025dcc1dd7c6fbbb88785b8844cd
2NgonNgon2 <tranvihao40@gmail.com>
2023-03-13 20:46:37 +0700

    update add to cart,fix responsive

--- model/insertProductToCart.php
+++ model/insertProductToCart.php
@@ -7,9 +7,15 @@
    $cItem_id = (int)$_GET['userId'];
    $cItem_name = $_GET['nameProduct'];
    $cItem_price = $_GET['priceProduct'];
-   $cItem_image = $_GET['imgSrc'];
-   //  $cItem_quantity = $_POST[''];
+   $cItem_image = $_GET['imgSrc']; 
    $cItem_quantity = 1;
+
+   $sql = "SELECT cItem_quantity FROM cart WHERE cUser_id = '{$cUser_id}' AND cItem_id = '{$cItem_id}'";
+   $result = $conn->query($sql);
+
+   if($result->num_rows > 0) {
+      $cItem_quantity += (int)$row['cItem_quantity'];
+   }
 
    $stmt = $conn->prepare("INSERT INTO cart (cUser_id, cItem_id, cItem_name,cItem_price,cItem_image,cItem_quantity) 
                            VALUES (?,?,?,?,?,?)");
@@ -20,6 +26,7 @@
    } else {
       echo "Please try again";
    }
+
    $stmt->execute();
 
    $stmt->close();
