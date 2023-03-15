01047c0db29306b9a56cfaea3b2752e8ba0402fc
2NgonNgon2 <tranvihao40@gmail.com>
2023-03-15 21:32:26 +0700

    update cart

--- model/insertProductToCart.php
+++ model/insertProductToCart.php
@@ -6,11 +6,11 @@
       echo "NotSignIn";
    } else {
 
-      /*  $cUser_id = $_POST['userId'] ; */
       $cUser_id = $_SESSION['accountId'];
       $cItem_id = (int)$_GET['userId'];
       $cItem_name = $_GET['nameProduct'];
-      $cItem_price = $_GET['priceProduct'];
+      $cItem_price_after_discount = $_GET['priceProductAfterDiscount'];
+      $cItem_price_before_discount = trim($_GET['priceProductBeforeDiscount'],"$");
       $cItem_image = $_GET['imgSrc']; 
       $cItem_quantity = 1;
 
@@ -24,9 +24,9 @@
          $stmt->bind_param("iii",$cItem_quantity,$cUser_id,$cItem_id);
       }
       else {
-         $stmt = $conn->prepare("INSERT INTO cart (cUser_id, cItem_id, cItem_name,cItem_price,cItem_image,cItem_quantity) 
-                                 VALUES (?,?,?,?,?,?)");
-         $stmt->bind_param("iisssi",$cUser_id,$cItem_id,$cItem_name,$cItem_price,$cItem_image,$cItem_quantity);
+         $stmt = $conn->prepare("INSERT INTO cart (cUser_id, cItem_id, cItem_name,cItem_price_after_discount,cItem_price_before_discount,cItem_image,cItem_quantity) 
+                                 VALUES (?,?,?,?,?,?,?)");
+         $stmt->bind_param("iissssi",$cUser_id,$cItem_id,$cItem_name,$cItem_price_after_discount,$cItem_price_before_discount,$cItem_image,$cItem_quantity);
       }
       if($stmt) {
          echo "You have added $cItem_name to your cart";
