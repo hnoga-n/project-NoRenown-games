c71828052933a6674a31984ee2a1693a11722827
hnoga-n <hoanghuynguyen711@gmail.com>
2023-04-28 16:12:04 +0700

    .

--- model/add_Game.php
+++ model/add_Game.php
@@ -11,11 +11,11 @@ $cfg_processor = $_POST['cfg_processor'];
 $cfg_graphics = $_POST['cfg_graphics'];
 $cfg_storage = $_POST['cfg_storage'];
 $about = $_POST['about'];
-$scr1 = $_POST['scr1'];
-$scr2 = $_POST['scr2'];
-$scr3 = $_POST['scr3'];
-$scr4 = $_POST['scr4'];
-$trailer = $_POST['trailer'];
+$scr1 = $_POST['gscr1'];
+$scr2 = $_POST['gscr2'];
+$scr3 = $_POST['gscr3'];
+$scr4 = $_POST['gscr4'];
+$trailer = $_POST['gtrailer'];
 
 $sql1 = "
             INSERT INTO games (gname,genreID,gprice,gdiscount,gimg,gquantity,trending,visible) VALUES
@@ -52,4 +52,3 @@ if ($result1 === TRUE) {
 } else {
     echo "Error: " . $sql1 . "<br>" . $conn->error . "<br>" . "Error: " . $sql1 . "<br>" . $conn->error;
 }
-?>
