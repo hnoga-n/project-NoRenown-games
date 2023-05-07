8057e1dc19d0b91bed7ef9e1f1b34911c4c96d0e
2NgonNgon2 <tranvihao40@gmail.com>
2023-05-07 08:55:18 +0700

    fix qunatity

--- model/change_order_status.php
+++ model/change_order_status.php
@@ -5,8 +5,23 @@
     $status = $_GET['status'];
     $orderID = $_GET['orderID'];
 
+    $selectOrderDetail = "SELECT * FROM invoice_detail JOIN games ON invoice_detail.gid = games.gid WHERE orderID = {$orderID}";
+    $result2 = $conn->query($selectOrderDetail);
+
+    while ($row = $result2->fetch_assoc()) {
+        
+        $sql2 = "SELECT gquantity FROM games WHERE gid = {$row['gid']}";
+        $result3 = $conn->query($sql2);
+        $row2 = $result3->fetch_assoc();
+
+        $quantity = (int) $row2['gquantity'] + (int) $row['quantity'];
+        $updateGameQuantity = "UPDATE games SET gquantity = $quantity WHERE games.gid = {$row['gid']}";
+        $conn->query($updateGameQuantity);
+    }
+
     $updateStatus = "UPDATE invoice SET order_status = {$status} WHERE accID = {$_COOKIE['accountId']} AND orderID = {$orderID}";
     $result = $conn->query($updateStatus);
 
-    echo "Cancel successful"
+    echo "Cancel successful";
+    $conn->close()
 ?>
