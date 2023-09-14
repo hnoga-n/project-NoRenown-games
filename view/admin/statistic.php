4d8c737477e3938658fec8927e1f2eced186b0d5
2NgonNgon2 <tranvihao40@gmail.com>
2023-09-14 21:20:27 +0700

    fix statistic,payment

--- view/admin/statistic.php
+++ view/admin/statistic.php
@@ -1,30 +1,35 @@
-<?php 
-  include_once "../../model/searchGenres.php";
-  
-  $accounts = "SELECT accid FROM account";
-  $result_accounts = $conn->query($accounts);
+<?php
+include_once "../../model/searchGenres.php";
 
-  $games = "SELECT gid FROM games";
-  $result_games = $conn->query($games);
+$accounts = "SELECT accid FROM account";
+$result_accounts = $conn->query($accounts);
 
-  $trash_games = "SELECT gid FROM games WHERE visible = 0";
-  $result_trash_games = $conn->query($trash_games);
+$games = "SELECT gid FROM games WHERE visible = 1";
+$result_games = $conn->query($games);
 
-  $invoices = "SELECT orderID FROM invoice";
-  $result_invoices = $conn->query($invoices);
+$trash_games = "SELECT gid FROM games WHERE visible = 0";
+$result_trash_games = $conn->query($trash_games);
 
-  $authorizations = "SELECT groupID FROM auth_group";
-  $result_authorizations = $conn->query($authorizations);
+$invoices = "SELECT orderID FROM invoice";
+$result_invoices = $conn->query($invoices);
+
+$authorizations = "SELECT groupID FROM auth_group";
+$result_authorizations = $conn->query($authorizations);
 
 ?>
 <html lang="en">
+
 <body>
   <div class="statistic-container">
     <div class="menu_bar">
       <div class="header_menu_bar">
         <div class="menu">
           <ul>
-          <li>Overview</li><li>Sold</li><li>Import</li><li>Customer</li><li>Category</li>
+            <li>Overview</li>
+            <li>Sold</li>
+            <li>Import</li>
+            <li>Customer</li>
+            <li>Category</li>
           </ul>
         </div>
       </div>
@@ -33,56 +38,56 @@
     <div id="container">
       <div class="statistic-container-header">
 
-          <div class="import_money">
-            <div class="content">
-              <div class="text">Accounts</div>
-              <div>
-                <div class="number" id="import-money"><?php echo $result_accounts->num_rows ?></div>
-                <i class="fa-solid fa-users-gear"></i>
-              </div>
-            </div>
-          </div>
-          <div class="earning">
-            <div class="content">
-              <div class="text">Invoices</div>
-              <div>
-                <div class="number" id="revenue"><?php echo $result_invoices->num_rows ?></div>
-                <i class="fa-solid fa-barcode"></i>
-              </div>
-            </div>
-          </div>
-          <div class="import_money">
-            <div class="content">
-              <div class="text">Games</div>
-              <div>
-                <div class="number" id="import-money"><?php echo $result_games->num_rows ?></div>
-                <i class="fa-solid fa-gamepad"></i>
-              </div>
-            </div>
-          </div>
-          <div class="import_quantity">
-            <div class="content">
-              <div class="text">Trash game</div>
-              <div>
-                <div class="number" id="import-quantity"><?php echo $result_trash_games->num_rows ?></div>
-                <i class="fa-solid fa-trash-can"></i>
-              </div>
-            </div>
-          </div>
-          <div class="import_quantity">
-            <div class="content">
-              <div class="text">Authorizations</div>
-              <div>
-                <div class="number" id="import-quantity"><?php echo $result_authorizations->num_rows ?></div>
-                <i class="fa-solid fa-screwdriver-wrench"></i>
-              </div>
+        <div class="import_money">
+          <div class="content">
+            <div class="text">Accounts</div>
+            <div>
+              <div class="number" id="import-money"><?php echo $result_accounts->num_rows ?></div>
+              <i class="fa-solid fa-users-gear"></i>
             </div>
           </div>
         </div>
-      
+        <div class="earning">
+          <div class="content">
+            <div class="text">Invoices</div>
+            <div>
+              <div class="number" id="revenue"><?php echo $result_invoices->num_rows ?></div>
+              <i class="fa-solid fa-barcode"></i>
+            </div>
+          </div>
+        </div>
+        <div class="import_money">
+          <div class="content">
+            <div class="text">Games</div>
+            <div>
+              <div class="number" id="import-money"><?php echo $result_games->num_rows ?></div>
+              <i class="fa-solid fa-gamepad"></i>
+            </div>
+          </div>
+        </div>
+        <div class="import_quantity">
+          <div class="content">
+            <div class="text">Trash game</div>
+            <div>
+              <div class="number" id="import-quantity"><?php echo $result_trash_games->num_rows ?></div>
+              <i class="fa-solid fa-trash-can"></i>
+            </div>
+          </div>
+        </div>
+        <div class="import_quantity">
+          <div class="content">
+            <div class="text">Authorizations</div>
+            <div>
+              <div class="number" id="import-quantity"><?php echo $result_authorizations->num_rows ?></div>
+              <i class="fa-solid fa-screwdriver-wrench"></i>
+            </div>
+          </div>
+        </div>
       </div>
+
     </div>
-  
+  </div>
+
 
 </body>
 
